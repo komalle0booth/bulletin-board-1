ff60c2cf54e789e4d03ec449c3593f0a2f82ab5f
komalle0booth <komalle0@chicagobooth.edu>
2024-08-06 05:12:07 +0000

    made active and expired, fixed form, made notice and alert - grade ready

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :expires_on, presence: true
+  validates :board_id, presence: true
 end
